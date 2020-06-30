41050bbc91a5f0a4484959fdccf45ec9af1fc604
ranyunlong <service@ranyunlong.com>
2020-06-30 21:36:16 +0800

    update:

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# @electron-runner/cli ![npm (scoped with tag)](https://img.shields.io/npm/v/@electron-runner/cli/latest)  ![GitHub commit activity](https://img.shields.io/github/commit-activity/m/leo-ran/electron-runner-cli)
+# @electron-runner/cli ![npm (scoped with tag)](https://img.shields.io/npm/v/@electron-runner/cli/latest) 
+
 基于 [webpack](https://webpack.js.org/) 和 [electron-builder](https://www.electron.build/) 的
 [electron](http://www.electronjs.org/) 项目运行工具，支持自动创建项目脚手架。
 
@@ -8,8 +9,10 @@
 > npm i -g @electron-runner/cli
 ```
 
-## 使用
+## 当前版本
+当前版本支持 React & Typescript，支持命令启动开发以及编译。
 
+## 使用
 ```shell
 # 创建项目
 > electron-runner create <project-name>
@@ -19,14 +22,6 @@
 # 编译
 > yarn build
 ```
-
-## 当前版本
-
-当前版本支持 React & Typescript， 支持命令启动开发以及编译。
-
-## 未来版本
-
-下个版本完成 Vue 的版本支持， Vue不考虑支持Typescript
 
 
 ## 社区
--- src/renderer-config.ts
+++ src/renderer-config.ts
@@ -10,228 +10,241 @@ import fs from "fs";
 import {ElectronRunnerConfig} from "./index";
 
 const isProduction = process.env.NODE_ENV === 'production';
-const buildType = (process.env.NODE_RENDERER_TYPE || "web") as  "web" | "electron-renderer";
+const buildType = (process.env.NODE_RENDERER_TYPE || "web") as "web" | "electron-renderer";
 const cwd = process.cwd();
 const sourcePath = path.join(cwd, "src", "renderer");
-const styleLoader = (isModule: boolean = false): RuleSetUseItem[] => {
-  return [
-    ...(isProduction ? [
-      MiniCssExtractPlugin.loader,
-    ] : []),
-    {
-      loader: "css-loader",
-      options: {
-        sourceMap: false,
-        modules: isModule,
-        import: isModule,
-      }
+const styleLoader = (isModule: boolean = false) => {
+    const cssLoader: any = {
+        loader: "css-loader",
+        options: {
+            sourceMap: false,
+            modules: isModule,
+            import: isModule,
+        }
     }
-  ];
+    if (isModule) {
+        cssLoader.options.localIdentName = '[path][name]__[local]--[hash:base64:5]';
+        cssLoader.options.camelCase = true;
+    }
+    return (
+        isProduction ?
+            [
+                MiniCssExtractPlugin.loader,
+                cssLoader
+            ] :
+            [
+                "style-loader",
+                cssLoader
+            ]
+    );
 };
 const lessLoaderOption: RuleSetUseItem = {
-  loader: "less-loader",
-  options: {
-    sourceMap: false,
-  }
+    loader: "less-loader",
+    options: {
+        sourceMap: false,
+    }
 }
 const sassLoaderOption: RuleSetUseItem = {
-  loader: "sass-loader",
-  options: {
-    implementation: require('sass'),
-    sourceMap: false,
-  }
+    loader: "sass-loader",
+    options: {
+        implementation: require('sass'),
+        sourceMap: false,
+    }
 }
 const devServerOption: DevServerConfiguration = {
-  compress: true,
-  port: 9080,
-  stats: isProduction ? "normal" : "errors-warnings",
+    compress: true,
+    port: 9080,
+    stats: isProduction ? "normal" : "errors-warnings",
 }
 
 let _rendererConfig: Configuration = {
-  mode: isProduction ? "production" : "development",
-  entry: [sourcePath],
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: styleLoader()
-      },
-      {
-        test: /\.module.css$/,
-        use: styleLoader(true)
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: [
-          "ts-loader",
-        ]
-      },
-      {
-        test: /\.m?jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            limit: 10000,
-            name: isProduction ? 'images/[name].[ext]' : 'images/[name].[hash:7].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            limit: 10000,
-            name: isProduction ? 'medias/[name].[ext]' : 'medias/[name].[hash:7].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            limit: 10000,
-            name: isProduction ? 'fonts/[name].[ext]' : 'fonts/[name].[hash:7].[ext]',
-          }
-        }
-      }
-    ],
-  },
-  output: {
-    path: path.join(cwd, "dist", "renderer"),
-    filename: `[name].js`,
-    libraryTarget: 'umd'
-  },
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-  plugins: [
-    // https://github.com/jantimon/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve('public', 'index.html'),
-      minify: isProduction,
-    }),
-    ...(
-      !isProduction ? [
-        new webpack.HotModuleReplacementPlugin(),
-      ] : [
-        // https://github.com/webpack-contrib/mini-css-extract-plugin
-        new MiniCssExtractPlugin({
-          filename: "styles/[name].css"
+    mode: isProduction ? "production" : "development",
+    entry: [sourcePath],
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: styleLoader()
+            },
+            {
+                test: /\.module.css$/,
+                use: styleLoader(true)
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    "ts-loader",
+                ]
+            },
+            {
+                test: /\.m?jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ],
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 10000,
+                        name: isProduction ? 'images/[name].[ext]' : 'images/[name].[hash:7].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 10000,
+                        name: isProduction ? 'medias/[name].[ext]' : 'medias/[name].[hash:7].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 10000,
+                        name: isProduction ? 'fonts/[name].[ext]' : 'fonts/[name].[hash:7].[ext]',
+                    }
+                }
+            }
+        ],
+    },
+    output: {
+        path: path.join(cwd, "dist", "renderer"),
+        filename: `[name].js`,
+        libraryTarget: 'umd'
+    },
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    plugins: [
+        // https://github.com/jantimon/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve('public', 'index.html'),
+            minify: isProduction,
         }),
-        // https://github.com/NMFR/optimize-css-assets-webpack-plugin
-        new OptimizeCssAssetsPlugin({})
-      ]
-    )
-  ],
-  optimization: {
-    minimize: isProduction,
-    splitChunks: isProduction ? {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
-    } : false
-  },
-  target: buildType,
-  stats: isProduction ? "normal" : "errors-warnings",
-  devServer: devServerOption,
+        ...(
+            !isProduction ? [
+                new webpack.HotModuleReplacementPlugin(),
+            ] : [
+                // https://github.com/webpack-contrib/mini-css-extract-plugin
+                new MiniCssExtractPlugin({
+                    filename: "styles/[name].css"
+                }),
+                // https://github.com/NMFR/optimize-css-assets-webpack-plugin
+                new OptimizeCssAssetsPlugin({})
+            ]
+        )
+    ],
+    optimization: {
+        minimize: isProduction,
+        splitChunks: isProduction ? {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        } : false
+    },
+    target: buildType,
+    stats: isProduction ? "normal" : "errors-warnings",
+    devServer: devServerOption,
 };
 const reConfigPath = path.join(cwd, "electron.config.js");
 
 function createLessConfig(lessLoaderOption: RuleSetUseItem) {
-  return [
-    {
-      test: /\.less$/,
-      use: [
-        ...styleLoader(),
-        lessLoaderOption,
-      ]
-    },
-    {
-      test: /\.module.less$/,
-      use: [
-        ...styleLoader(true),
-        lessLoaderOption,
-      ]
-    },
-  ]
+    return [
+        {
+            test: /\.less$/,
+            use: [
+                ...styleLoader(),
+                lessLoaderOption,
+            ]
+        },
+        {
+            test: /\.module.less$/,
+            use: [
+                ...styleLoader(true),
+                lessLoaderOption,
+            ]
+        },
+    ]
 }
 
 function createSassConfig(sassLoaderOption: RuleSetUseItem) {
-  return [
-    {
-      test: /\.s[ac]ss$/i,
-      use: [
-        ...styleLoader(),
-        sassLoaderOption,
-      ]
-    },
-    {
-      test: /\.module.s[ac]ss$/i,
-      use: [
-        ...styleLoader(true),
-        sassLoaderOption,
-      ]
-    },
-  ]
+    return [
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+                ...styleLoader(),
+                sassLoaderOption,
+            ]
+        },
+        {
+            test: /\.module.s[ac]ss$/i,
+            use: [
+                ...styleLoader(true),
+                sassLoaderOption,
+            ]
+        },
+    ]
 }
 
 if (_rendererConfig.module) {
-  if (fs.existsSync(reConfigPath)) {
-    const config: ElectronRunnerConfig = require(reConfigPath);
-    if (typeof config === "object") {
-      if (typeof config.less === "function") {
-        _rendererConfig.module.rules.push(...createLessConfig(config.less(lessLoaderOption)))
-      }
+    if (fs.existsSync(reConfigPath)) {
+        const config: ElectronRunnerConfig = require(reConfigPath);
+        if (typeof config === "object") {
+            if (typeof config.less === "function") {
+                _rendererConfig.module.rules.push(...createLessConfig(config.less(lessLoaderOption)))
+            }
 
-      if (typeof config.sass === "function") {
-        _rendererConfig.module.rules.push(...createSassConfig(config.sass(sassLoaderOption)))
-      }
+            if (typeof config.sass === "function") {
+                _rendererConfig.module.rules.push(...createSassConfig(config.sass(sassLoaderOption)))
+            }
 
-      if (config.devServer) {
-        _rendererConfig.devServer = {
-          ..._rendererConfig.devServer,
-          ...config.devServer,
+            if (config.devServer) {
+                _rendererConfig.devServer = {
+                    ..._rendererConfig.devServer,
+                    ...config.devServer,
+                }
+            }
+
+            if (config.webpack) {
+                _rendererConfig = merge(_rendererConfig, config.webpack)
+            }
+        } else {
+            _rendererConfig.module.rules.push(
+                ...createLessConfig(lessLoaderOption),
+                ...createSassConfig(sassLoaderOption)
+            )
         }
-      }
-
-      if (config.webpack) {
-        _rendererConfig = merge(_rendererConfig, config.webpack)
-      }
     } else {
-      _rendererConfig.module.rules.push(
-        ...createLessConfig(lessLoaderOption),
-        ...createSassConfig(sassLoaderOption)
-      )
+        _rendererConfig.module.rules.push(
+            ...createLessConfig(lessLoaderOption),
+            ...createSassConfig(sassLoaderOption)
+        )
     }
-  } else {
-    _rendererConfig.module.rules.push(
-      ...createLessConfig(lessLoaderOption),
-      ...createSassConfig(sassLoaderOption)
-    )
-  }
 }
 
 export const rendererConfig = _rendererConfig;
